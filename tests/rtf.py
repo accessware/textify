82b0b196f0c2204c3a5b205826f7627115f644a7
Quin Marilyn <quin.marilyn05@gmail.com>
2021-09-24 10:50:16 -0600

    Made the test take commandline args. I need to give this thing it'sown cli interface.

--- tests/rtf.py
+++ tests/rtf.py
@@ -2,7 +2,14 @@ import sys
 sys.path.append("..")
 import textify
 
-file = open("rtf.txt", "w")
-text = textify.textify("test.rtf")
-file.write(text)
-file.close()
+if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		print("Usage: rtf.py <input_file> <output_file>")
+		sys.exit()
+	input_file = sys.argv[1]
+	output_file = sys.argv[2]
+	file = open(output_file, "w")
+	text = textify.textify(input_file)
+	file.write(text)
+	file.close()
+	print("Done!")
